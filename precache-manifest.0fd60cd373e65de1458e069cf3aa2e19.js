9b84edcbd9ae5530aa41442b1ac0c9c524ee0778
MRMSalon <68626012+MRMSalon@users.noreply.github.com>
2020-07-21 21:48:40 -0500

    Initial website

---
+++ precache-manifest.0fd60cd373e65de1458e069cf3aa2e19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf215e24dd8a87b14ed672f1bfd8360f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ca062f6a526eb6432c",
+    "url": "/static/css/main.1f2d4078.chunk.css"
+  },
+  {
+    "revision": "9cd735531a862fb692d5",
+    "url": "/static/js/2.4c644c20.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4c644c20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ca062f6a526eb6432c",
+    "url": "/static/js/main.f833eac4.chunk.js"
+  },
+  {
+    "revision": "c3f92f81eed3e5175c24",
+    "url": "/static/js/runtime-main.9157843e.js"
+  }
+]);
